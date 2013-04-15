297cca30fa78344bc9bbda2fce888dce73aa5cf1
Chris Loew <cloew123@gmail.com>
2013-04-15 14:25:42 -0600

    Added mine class

---
+++ src/mine.py
@@ -0,0 +1,13 @@
+
+class Mine:
+    """ Represents a Mine in the Game """
+    
+    def __init__(self):
+        """ Initialize the Mine """
+        self.defused = False
+        
+    def scan(self):
+        """ Scan the mine and destroy the drone """
+        
+    def defuse(self):
+        """ Defuse the Mine """
