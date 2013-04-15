acc83b5d8660ae930cae0d69193c7af0f8e496d0
Chris Loew <cloew123@gmail.com>
2013-04-15 12:55:07 -0600

    Added Minefield View

---
+++ src/View/Console/minefield_view.py
@@ -0,0 +1,24 @@
+
+class MinefieldView:
+    """ Represents the View of the Minefield """
+    
+    def __init__(self, field, drone):
+        """ Initialize the Minefield View """
+        self.field = field
+        self.drone = drone
+        
+    def __repr__(self):
+        """ Print the string representation of the Minefield View """
+        fieldString = ""
+        for row in self.field.squares:
+            for square in row:
+                fieldString += self.getSquareString(square)
+            fieldString += "\n"
+        return fieldString
+                
+    def getSquareString(self, square):
+        """ Get Square String """
+        if square.row == self.drone.row and square.column == self.drone.column:
+            return "[{0}]".format(square)
+        else:
+            return " {0} ".format(square)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
+from drone import Drone
 from minefield import Minefield
+
+from View.Console.minefield_view import MinefieldView
 
 import sys
 
 def main(args):
     """ Run the main file """
     field = Minefield()
-    print field
+    drone = Drone()
+    
+    field_view = MinefieldView(field, drone)
+    print field_view
 
 if __name__ == "__main__":
     main(sys.argv[1:])
