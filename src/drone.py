9c507ac0731d99f20cd714244ca7c5ae8e43efcc
Chris Loew <cloew123@gmail.com>
2013-04-15 12:58:54 -0600

    Added functions to move the drone ship

--- src/drone.py
+++ src/drone.py
@@ -6,3 +6,19 @@ class Drone:
         """ Intialize the Player's Drone """
         self.row = 0
         self.column = 0
+        
+    def up(self, field):
+        """  """
+        self.row -= 1
+        
+    def down(self, field):
+        """ """
+        self.row += 1
+    
+    def left(self, field):
+        """ """
+        self.column -= 1
+    
+    def right(self, field):
+        """ """
+        self.column += 1
