1bb80727064188c1529bd7b78b3f96d99458550b
Chris Loew <cloew123@gmail.com>
2013-04-15 12:30:18 -0600

    Added the Grid Square class

---
+++ src/grid_square.py
@@ -0,0 +1,11 @@
+
+class GridSquare:
+    """ Represents a square in the Minefield Grid """
+    
+    def __init__(self):
+        """ Initialize the Grid Square """
+        
+        
+    def __repr__(self):
+        """ Print the square as a string """
+        return "#"
--- src/minefield.py
+++ src/minefield.py
@@ -1,3 +1,4 @@
+from grid_square import GridSquare
 
 class Minefield:
     """ Represents the Minefield for the current level """
@@ -9,7 +10,7 @@ class Minefield:
         for row in range(4):
             self.squares.append([])
             for column in range(5):
-                self.squares[row].append(None)
+                self.squares[row].append(GridSquare())
         
     def __repr__(self):
         """ Print the String representation of the Minefield """
