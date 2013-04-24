eac1ce587fe7eb3dd03b3e180fe961468d8eadcc
Chris Loew <cloew123@gmail.com>
2013-04-23 19:10:13 -0600

    Got the Clue View to appear properly

--- src/View/Qt/clue_view.py
+++ src/View/Qt/clue_view.py
@@ -1,16 +1,37 @@
 from PySide.QtCore import Qt
-from PySide.QtGui import QFont
+from PySide.QtGui import QFont, QFrame, QLabel
 
-class ClueView:
+class ClueView(QFrame):
     """ Represents the Graphical view of the Drone """
 
-    def __init__(self, clue):
+    def __init__(self, clue, parent=None):
         """ Initialize the drone view """
+        QFrame.__init__(self, parent)
         self.clue = clue
+        
+        self.setupClueLabel()
+        self.resize(64, 64)
+        
+    def setupFont(self):
+        """ Setup the Font """
+        self.font = QFont()
+        self.font.setPointSize(40)
+        
+    def setupClueLabel(self):
+        """ Setup the Clue Label """
+        self.setupFont()
+        self.clueLabel = QLabel(str(self.clue), self)
+        self.clueLabel.setFont(self.font)
+        self.clueLabel.move(18, 0)
+        
+    def updateView(self):
+        """ Update the View """
+        self.clueLabel.setText(str(self.clue))
+        self.clueLabel.resize(34, self.clueLabel.contentsRect().height())
+        #self.clueLabel.resize(100, 30)
+        #self.clueLabel.move(20, 0)
 
     def draw(self, painter, gridSquareView):
         """ Draw the image """
-        font = QFont()
-        font.setPointSize(54)
-        painter.setFont(font)
+        #painter.setFont(font)
         painter.drawText(gridSquareView.getXCoordinate()+10, gridSquareView.getYCoordinate()+54, str(self.clue))
--- src/View/Qt/grid_square_view.py
+++ src/View/Qt/grid_square_view.py
@@ -17,14 +17,15 @@ class GridSquareView(QFrame):
         
         self.scaled_grid_square = None
         self.scaled_scanned_grid_square = None
+        self.clueView = ClueView(self.gridSquare.clue, self)
         self.loadGridSquareImage()
         
-        self.clueView = ClueView(self.gridSquare.clue)
         if self.gridSquare.mined():
             self.mineView = MineView(self.gridSquare.contents[0])
             
         self.resize(64, 64)
         self.move(self.getXCoordinate(), self.getYCoordinate())
+        self.clueView.raise_()
 
     def loadGridSquareImage(self):
         """ Load the grid square image """
@@ -36,6 +37,7 @@ class GridSquareView(QFrame):
         self.grid_square_label.setPixmap(self.grid_square_pixmap)
         self.grid_square_label.move(0, 0)
         self.grid_square_label.setVisible(True)
+        self.grid_square_label.stackUnder(self.clueView)
         
         self.unscaled_scanned_grid_square = QImage("ScannedGridSquare.png")
         self.scaled_scanned_grid_square = self.unscaled_scanned_grid_square.scaled(self.GRID_SQUARE_SIZE, self.GRID_SQUARE_SIZE)
@@ -45,18 +47,20 @@ class GridSquareView(QFrame):
         self.scanned_grid_square_label.setPixmap(self.scanned_grid_square_pixmap)
         self.scanned_grid_square_label.move(0, 0)
         self.scanned_grid_square_label.setVisible(False)
+        self.scanned_grid_square_label.stackUnder(self.clueView)
         
     def updateView(self):
         """ Update the Grid Square View """
         if self.gridSquare.scanned:
             self.grid_square_label.setVisible(False)
             self.scanned_grid_square_label.setVisible(True)
+            self.clueView.updateView()
 
     def draw(self, painter):
         """ Draw the image """
         self.drawGridSquareBackground(painter)
-        self.drawMine(painter)
-        self.drawClue(painter)
+        #self.drawMine(painter)
+        #self.drawClue(painter)
             
     def drawGridSquareBackground(self, painter):
         """ Draw the Grid Square Background """
