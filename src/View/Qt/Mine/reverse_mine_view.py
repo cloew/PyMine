904ebf5d6372c9bdf8c1023704010b16cb27269f
Chris Loew <cloew123@gmail.com>
2013-04-27 10:06:04 -0600

    Added a view for the reverse Mine

---
+++ src/View/Qt/Mine/reverse_mine_view.py
@@ -0,0 +1,6 @@
+from mine_view import MineView
+
+class ReverseMineView(MineView):
+    """ View for a Reverse Mine """
+    DEFUSED_IMAGE_FILENAME = "DefusedReverseMine.png"
+    REGULAR_IMAGE_FILENAME = "ReverseMine.png"
