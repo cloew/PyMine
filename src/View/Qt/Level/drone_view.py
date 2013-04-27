7c129c4856b3f31c389ae4a563799c5be55295a6
Chris Loew <cloew123@gmail.com>
2013-04-27 01:32:43 -0600

    Drone View now uses the image loader module

--- src/View/Qt/Level/drone_view.py
+++ src/View/Qt/Level/drone_view.py
@@ -1,7 +1,7 @@
 from grid_square_view import GridSquareView
+from View.Qt.image_loader import LoadImageLabel
 
-from PySide.QtCore import Qt
-from PySide.QtGui import QFrame, QImage, QLabel, QMatrix, QPixmap
+from PySide.QtGui import QFrame
 
 class DroneView(QFrame):
     """ Represents the Graphical view of the Drone """
@@ -11,20 +11,10 @@ class DroneView(QFrame):
         QFrame.__init__(self, parent)
         self.drone_model = drone_model
         
-        self.scaled_drone = None
         self.loadDroneImage()
         self.resize(64, 64)
 
     def loadDroneImage(self):
         """ Load the drone image """
-        self.unscaled_drone = QImage("Drone2Clear.png")
-        self.scaled_drone = self.unscaled_drone.scaled(GridSquareView.GRID_SQUARE_SIZE, GridSquareView.GRID_SQUARE_SIZE)
-        self.drone_pixmap = QPixmap()
-        self.drone_pixmap.convertFromImage(self.scaled_drone)
-        self.drone_label = QLabel(self)
-        self.drone_label.setPixmap(self.drone_pixmap)
+        self.drone_label = LoadImageLabel("Drone2Clear.png", scaledXSize=GridSquareView.GRID_SQUARE_SIZE, scaledYSize=GridSquareView.GRID_SQUARE_SIZE, parent=self)
         self.drone_label.move(0, 0)
-
-    def draw(self, painter, minefieldView):
-        """ Draw the image """
-        painter.drawImage(minefieldView.x+self.drone_model.column*GridSquareView.GRID_SQUARE_SIZE, minefieldView.y+self.drone_model.row*GridSquareView.GRID_SQUARE_SIZE, self.scaled_drone)
