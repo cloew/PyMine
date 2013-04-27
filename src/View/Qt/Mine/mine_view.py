12be1be5ee50747919a78dddbd12dc77aed24873
Chris Loew <cloew123@gmail.com>
2013-04-27 10:03:48 -0600

    Made the filenames to load the MIne images members of the class in preparation for the various mine types having their own appearnaces

--- src/View/Qt/Mine/mine_view.py
+++ src/View/Qt/Mine/mine_view.py
@@ -2,7 +2,9 @@ from View.Qt.Level.grid_square_frame import GridSquareFrame
 
 class MineView(GridSquareFrame):
     """ Represents the Graphical view of a Mine """
-
+    DEFUSED_IMAGE_FILENAME = "DefusedMine1.png"
+    REGULAR_IMAGE_FILENAME = "Mine1.png"
+    
     def __init__(self, mine, parent=None):
         """ Initialize the mine view """
         GridSquareFrame.__init__(self, parent)
@@ -12,8 +14,8 @@ class MineView(GridSquareFrame):
 
     def loadMineImages(self):
         """ Load the Mine images """
-        self.mine_label = self.loadGridSquareSizedImage("Mine1.png")
-        self.defused_mine_label = self.loadGridSquareSizedImage("DefusedMine1.png", visible=False)
+        self.mine_label = self.loadGridSquareSizedImage(self.REGULAR_IMAGE_FILENAME)
+        self.defused_mine_label = self.loadGridSquareSizedImage(self.DEFUSED_IMAGE_FILENAME, visible=False)
 
     def updateView(self):
         """  """
