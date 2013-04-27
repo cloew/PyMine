81faafb4630fe4ad7ab2f372ed81916fcac4fb1c
Chris Loew <cloew123@gmail.com>
2013-04-27 16:36:14 -0600

    Added a level with more than 4 rows and 5 columns

--- src/Level/level_selection.py
+++ src/Level/level_selection.py
@@ -12,7 +12,8 @@ class LevelSelection:
         self.levels = [Level("Level 1", 4, 5, {Mine:4}), 
                        Level("Level 2", 4, 5, {Mine:4, ReverseMine:1}), 
                        Level("Level 3", 4, 5, {Mine:10, ReverseMine:2}),
-                       Level("Level 4", 4, 5, {Mine:8, ReverseMine:2, FragileMine:1})]
+                       Level("Level 4", 4, 5, {Mine:8, ReverseMine:2, FragileMine:1}),
+                       Level("Level 5", 5, 6, {Mine:8, ReverseMine:2, FragileMine:1})]
         self.selectedIndex = 0
         
     def performGameCycle(self):
