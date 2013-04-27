b83899b6cf6fe3abc3837f4b6d63d11b0f4063fc
Chris Loew <cloew123@gmail.com>
2013-04-27 02:00:16 -0600

    Drone View now inherits from the GridSquareFrame

--- src/View/Qt/Level/drone_view.py
+++ src/View/Qt/Level/drone_view.py
@@ -1,20 +1,13 @@
 from grid_square_view import GridSquareView
 from View.Qt.image_loader import LoadImageLabel
+from View.Qt.Level.grid_square_frame import GridSquareFrame
 
-from PySide.QtGui import QFrame
-
-class DroneView(QFrame):
+class DroneView(GridSquareFrame):
     """ Represents the Graphical view of the Drone """
 
     def __init__(self, drone_model, parent=None):
         """ Initialize the drone view """
-        QFrame.__init__(self, parent)
+        GridSquareFrame.__init__(self, parent)
         self.drone_model = drone_model
         
-        self.loadDroneImage()
-        self.resize(64, 64)
-
-    def loadDroneImage(self):
-        """ Load the drone image """
-        self.drone_label = LoadImageLabel("Drone2Clear.png", scaledXSize=GridSquareView.GRID_SQUARE_SIZE, scaledYSize=GridSquareView.GRID_SQUARE_SIZE, parent=self)
-        self.drone_label.move(0, 0)
+        self.drone_label = self.loadGridSquareSizedImage("Drone2Clear.png")
