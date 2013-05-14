dc7f2310d40117d9fd0f21c383e70adae0c814ce
Chris Loew <cloew123@gmail.com>
2013-05-14 10:38:30 -0600

    Got the Defenses to appear in a consistent order

--- src/View/Qt/LevelSelection/level_details_view.py
+++ src/View/Qt/LevelSelection/level_details_view.py
@@ -9,6 +9,7 @@ from PySide.QtGui import QColor, QFont, QFrame, QLabel
 
 class LevelDetailsView(QFrame):
     """ Represents the View of the Level Details """
+    DEFENSE_ORDER = [Mine, ReverseMine, FragileMine, Worm, AntiDroneMine]
 
     def __init__(self, levelSelection, parent=None):
         """ Initialize the Level Details View """
@@ -23,45 +24,43 @@ class LevelDetailsView(QFrame):
         self.bigFont = QFont()
         self.bigFont.setPointSize(24)
         
-        self.detailsLabel = self.getLabel("Level Details", self.bigFont)
+        self.detailsLabel = self.getLabel("Level Details", self.bigFont, alignment=Qt.AlignCenter)
         self.detailsLabel.move(32, 16)
         self.setupDefensesLabels()
         
-    def getLabel(self, text, font):
+    def getLabel(self, text, font, alignment=None):
         """ Get a Level Label with the text """
         label = QLabel(text, self)
         label.setFont(font)
-        label.setAlignment(Qt.AlignCenter)
+        if alignment:
+            label.setAlignment(alignment)
         return label
         
     def setupDefensesLabels(self):
         """ Setup the Remaining Defenses Labels """
         self.defensesLabels = {}
         
-        for defenseClass in [AntiDroneMine, Mine, FragileMine, ReverseMine, Worm]:
+        for defenseClass in self.DEFENSE_ORDER:
             label = self.getLabel("", self.smallFont)
             label.setVisible(False)
+            label.resize(200, 32)
             self.defensesLabels[defenseClass] = label
         
     def updateView(self):
         """ Update the View """
-        self.hideDefenseLabels()
         self.updateDefenseLabels()
         self.update()
-        
-    def hideDefenseLabels(self):
-        """ Hide Defense Labels """
-        for defenseClass in self.defensesLabels:
-            label = self.defensesLabels[defenseClass]
-            label.setVisible(False)
         
     def updateDefenseLabels(self):
         """ Update the Defense Label """
         defenses = self.levelSelection.getLevel().defenses
         labelNumber = 1
-        for defenseClass in defenses:
+        for defenseClass in self.DEFENSE_ORDER:
             label = self.defensesLabels[defenseClass]
-            label.setVisible(True)
-            label.setText("{0}: {1}".format(defenseClass.friendlyName, defenses[defenseClass]))
-            label.move(32, 16+32*(labelNumber+1))
-            labelNumber += 1
+            if defenseClass in defenses:
+                label.setVisible(True)
+                label.setText("{0}: {1}".format(defenseClass.friendlyName, defenses[defenseClass]))
+                label.move(32, 16+32*(labelNumber+1))
+                labelNumber += 1
+            else:
+                label.setVisible(False)
