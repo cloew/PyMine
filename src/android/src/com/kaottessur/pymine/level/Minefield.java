4507e5cf54ddaf558de49736f0a5982c61f26082
Chris Loew <cloew123@gmail.com>
2013-05-17 16:01:16 -0600

    Added Minefield class

---
+++ src/android/src/com/kaottessur/pymine/level/Minefield.java
@@ -0,0 +1,12 @@
+package com.kaottessur.pymine.level;
+
+public class Minefield {
+	private int rows;
+	private int columns;
+	
+	
+	public Minefield(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+	}
+}
