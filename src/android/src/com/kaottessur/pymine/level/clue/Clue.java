b3ddd01cd92f0b968a43e62ebf52c09575cde543
Chris Loew <cloew123@gmail.com>
2013-05-20 12:00:40 -0600

    Started stubbing out Clue class and logic

--- src/android/src/com/kaottessur/pymine/level/clue/Clue.java
+++ src/android/src/com/kaottessur/pymine/level/clue/Clue.java
@@ -1,8 +1,29 @@
 package com.kaottessur.pymine.level.clue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kaottessur.pymine.level.GridSquare;
+import com.kaottessur.pymine.level.Minefield;
+
 public class Clue {
+	private AdjacencyClue adjacencyClue;
+	private List<SubClue> subClues;
 	
 	public Clue() {
+		subClues = new ArrayList<SubClue>();
+		adjacencyClue = new AdjacencyClue();
+		subClues.add(adjacencyClue);
+	}
+	
+	public void populate(Minefield minefield, int gridRow, int gridColumn) {
+		for (SubClue subClue : subClues)
+			subClue.update();
+		populateFromAdjacentGridSquares(minefield, gridRow, gridColumn);
+	}
+	
+	public void populateFromAdjacentGridSquares(Minefield minefield, int gridRow, int gridColumn) {
+		GridSquare currentSquare = minefield.getGridSquareAt(gridRow, gridColumn);
 		
 	}
 }
