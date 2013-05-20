5542f8f3534f97c24020894213679fb38e2307c0
Chris Loew <cloew123@gmail.com>
2013-05-19 23:39:53 -0600

    Added stubbed out Standard Defense Adder

---
+++ src/android/src/com/kaottessur/pymine/defense/adder/StandardDefenseAdder.java
@@ -0,0 +1,16 @@
+package com.kaottessur.pymine.defense.adder;
+
+public class StandardDefenseAdder {
+	
+	 public StandardDefenseAdder() {
+		 
+	 }
+	 
+	 public void addDefense() {
+		 
+	 }
+	 
+	 public void getRandomGridSquare() {
+		 
+	 }
+}
