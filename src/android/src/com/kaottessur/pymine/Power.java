096a984577281c4af5da53dfc01bc847cdb710f2
Chris Loew <cloew123@gmail.com>
2013-05-21 15:57:16 -0600

    Added Power constants

---
+++ src/android/src/com/kaottessur/pymine/Power.java
@@ -0,0 +1,9 @@
+package com.kaottessur.pymine;
+
+public class Power {
+	public static final int SCAN_POWER = 2;
+	public static final int DEFUSE_POWER = 5;
+	public static final int CAREFUL_DEFUSE_POWER = 25;
+	public static final int EMP_POWER = 15;
+
+}
