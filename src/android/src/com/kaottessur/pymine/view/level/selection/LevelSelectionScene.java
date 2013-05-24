3a4d127fcd0ab75f14a2453a0c87449ba2d22074
Chris Loew <cloew123@gmail.com>
2013-05-24 15:04:06 -0600

    Replaced rectangle on the main Level Selection with a Button

--- src/android/src/com/kaottessur/pymine/view/level/selection/LevelSelectionScene.java
+++ src/android/src/com/kaottessur/pymine/view/level/selection/LevelSelectionScene.java
@@ -1,37 +1,29 @@
 package com.kaottessur.pymine.view.level.selection;
 
-import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
-import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
 import com.kaottessur.pymine.level.selection.LevelSelection;
+import com.kaottessur.pymine.view.Button;
 import com.kaottessur.pymine.view.SceneManager;
 
 public class LevelSelectionScene extends Scene {
-	//private final LevelSelection levelSelection;
 	
 	public LevelSelectionScene(final LevelSelection levelSelection, VertexBufferObjectManager vertexBufferObjectManager) {
 		super();
-		//this.levelSelection = levelSelection;
 		
 		LevelOverviewText overview = new LevelOverviewText(levelSelection, 0, vertexBufferObjectManager);
 		attachChild(overview);
 		registerTouchArea(overview);
 		
-		Rectangle playButton = new Rectangle(400, 240, 100, 100, vertexBufferObjectManager)
+		Button playButton = new Button(400, 240, 100, 100, vertexBufferObjectManager)
 		{
-	        public boolean onAreaTouched(TouchEvent touchEvent, float X, float Y)
+	        public void performButtonAction()
 	        {
-	            if (touchEvent.isActionUp())
-	            {
-	            	SceneManager.GetInstance().runLevelScene(levelSelection.getSelectedLevel());
-	            }
-	            return true;
+	            SceneManager.GetInstance().runLevelScene(levelSelection.getSelectedLevel());
 	        };
 	    };
-		playButton.setColor(.5f, .5f, .5f);
 		attachChild(playButton);
 		registerTouchArea(playButton);
 		
