cb13fd85d6296562cb3b3342215eabe100c0cc86
Chris Loew <cloew123@gmail.com>
2013-06-10 21:15:36 -0600

    Fragile Mines now have the proper recommended power level

--- src/android/src/com/kaottessur/pymine/defense/FragileMine.java
+++ src/android/src/com/kaottessur/pymine/defense/FragileMine.java
@@ -1,11 +1,17 @@
 package com.kaottessur.pymine.defense;
 
+import com.kaottessur.pymine.Power;
 import com.kaottessur.pymine.level.Drone;
 
 public class FragileMine extends Mine {
 	@Override
 	public boolean isFragile() {
 		return true;
+	}
+	
+	@Override
+	public int getPowerRating() {
+		return Power.CAREFUL_DEFUSE_POWER;
 	}
 	
 	@Override
