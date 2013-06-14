0bd6184a7f7aca4bab0febd97004e9d5dea64df3
Chris Loew <cloew123@gmail.com>
2013-06-14 17:20:01 -0600

    Added Ajacency CLue Text to pull some of that specific clue logic out of the clue view

---
+++ src/android/src/com/kaottessur/pymine/view/level/clue/AdjacencyClueText.java
@@ -0,0 +1,51 @@
+package com.kaottessur.pymine.view.level.clue;
+
+import org.andengine.engine.handler.IUpdateHandler;
+import org.andengine.entity.text.Text;
+
+import com.kaottessur.pymine.level.clue.Clue;
+import com.kaottessur.pymine.view.SceneManager;
+import com.kaottessur.pymine.view.texture.TextureWrapper;
+
+public class AdjacencyClueText extends Text {
+	private Clue clue;
+	
+	public AdjacencyClueText(Clue clue) {
+		super(40, 0, TextureWrapper.GetInstance().GetGameFont(), "", 1, SceneManager.GetVertexBufferObjectManager());
+		this.clue = clue;
+		registerUpdateHandler(new IUpdateHandler() {
+			
+			@Override
+			public void reset() {
+				// Do Nothing
+			}
+			
+			@Override
+			public void onUpdate(float pSecondsElapsed) {
+				updateClueViewText();
+				updateTextColor();
+			}
+		});
+	}
+	
+	public void updateClueViewText() {
+		int numberOfAdjacentMines = clue.getAdjacencyClue().getClueReading();
+		if (numberOfAdjacentMines > 0)
+			setText(Integer.toString(numberOfAdjacentMines));
+		else
+			setText("");
+	}
+	
+	private void updateTextColor() {
+		int distance = clue.getFragilityClue().getDistance();
+		if (distance == -1) {
+			setColor(0, 0, 0);
+		} else if (distance <= 1) {
+			setColor((float) 244.0/255, 0, 0);
+		} else if (distance < 4) {
+			setColor((float) 244.0/255, (float) 244.0/255, 0);
+		} else {
+			setColor(0, (float) 154.0/255, 0);
+		}
+	}
+}
