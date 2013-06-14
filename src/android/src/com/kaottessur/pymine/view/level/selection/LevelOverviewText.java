2af1da6c19d6a2b54301f0b2d993a86ddd58f1d9
Chris Loew <cloew123@gmail.com>
2013-06-13 22:05:51 -0600

    Level Names now appear again

--- src/android/src/com/kaottessur/pymine/view/level/selection/LevelOverviewText.java
+++ src/android/src/com/kaottessur/pymine/view/level/selection/LevelOverviewText.java
@@ -3,7 +3,6 @@ package com.kaottessur.pymine.view.level.selection;
 import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.text.Text;
 import org.andengine.input.touch.TouchEvent;
-import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
 import com.kaottessur.pymine.level.selection.LevelSelection;
 import com.kaottessur.pymine.view.SceneManager;
@@ -12,14 +11,19 @@ import com.kaottessur.pymine.view.texture.TextureWrapper;
 public class LevelOverviewText extends Rectangle {
 	private LevelSelection levelSelection;
 	private int index;
+	private Text nameText;
 	
 	
 	public LevelOverviewText(LevelSelection levelSelection, int index) {
 		super(50+100*(index%3), 50+100*(index/3), 50, 50, SceneManager.GetVertexBufferObjectManager());
-		//super(50+100*(index%3), 50+100*(index/3), TextureWrapper.GetInstance().GetCompletionFont(), levelSelection.getLevelAt(index).getName(), vertexBufferObjectManager);
+		
+		nameText = new Text(0, 0, TextureWrapper.GetInstance().GetCompletionFont(), levelSelection.getLevelAt(index).getName(), SceneManager.GetVertexBufferObjectManager());
+		attachChild(nameText);
 		
 		this.levelSelection = levelSelection;
 		this.index = index;
+		
+		setColor(.75f, .75f, .75f);
 	}
 	
 	@Override
